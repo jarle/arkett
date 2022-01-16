b9e49b65e0dccb2ec31e7a6834a32a2719cfc71e
jarle.mathiesen@gmail.com <jarle.mathiesen@gmail.com>
2022-01-16 13:15:48 +0100

    bump react-quill to beta

--- src/components/ConfiguredEditor.js
+++ src/components/ConfiguredEditor.js
@@ -1,7 +1,11 @@
 import ReactQuill from 'react-quill';
 
 export default function ConfiguredEditor(props) {
-
-    return <ReactQuill theme={'bubble'} bounds={`[data-text-editor="arkett-editor"]`} {...props} />;
-
+    return (
+        <ReactQuill
+            theme={'bubble'}
+            bounds={`[data-text-editor="arkett-editor"]`}
+            {...props}
+        />
+    )
 }
